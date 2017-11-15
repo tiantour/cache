0d8b894f1c81669e4fcf774c4a00cbb863d09cbc
姜灏 <150haomengting@163.com>
2017-11-15 18:59:17 +0800

    remove conf

--- cache.go
+++ cache.go
@@ -5,10 +5,11 @@ import (
 
 	"github.com/mediocregopher/radix.v2/pool"
 	"github.com/mediocregopher/radix.v2/redis"
-	"github.com/tiantour/conf"
 )
 
 var (
+	IP     = "127.0.0.1"
+	Port   = "6379"
 	p, err = new()
 )
 
@@ -17,7 +18,7 @@ func new() (*pool.Pool, error) {
 	df := func(network, addr string) (*redis.Client, error) {
 		return redis.Dial(network, addr)
 	}
-	account := fmt.Sprintf("%s:%s", conf.NewConf().Cache.Host, conf.NewConf().Cache.Port)
+	account := fmt.Sprintf("%s:%s", IP, Port)
 	return pool.NewCustom("tcp", account, 10, df)
 }
 
