85512945bdc3303109c69874b37b721ea860b301
andy.jiang <150haomengting@163.com>
2022-03-02 15:41:23 +0800

    fix bugs

--- cache.go
+++ cache.go
@@ -10,17 +10,26 @@ import (
 	"github.com/tiantour/conf"
 )
 
-var address = fmt.Sprintf("%s%s", conf.NewCache().Data.IP, conf.NewCache().Data.Port)
+var (
+	client  radix.Client
+	address = fmt.Sprintf("%s%s", conf.NewCache().Data.IP, conf.NewCache().Data.Port)
+)
+
+func init() {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var err error
+	client, err = (radix.PoolConfig{}).New(ctx, "tcp", address)
+	if err != nil {
+		defer client.Close()
+		log.Fatalf("open cache err: %v", err)
+	}
+}
 
 func do(args radix.Action) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-
-	client, err := (radix.PoolConfig{}).New(ctx, "tcp", address)
-	if err != nil {
-		log.Fatalf("open cache err: %v", err)
-	}
-	defer client.Close()
 
 	return client.Do(ctx, args)
 }
